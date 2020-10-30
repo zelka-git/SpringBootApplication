0ef182f563627d3574f0c55bcfe1faffb1779799
Anzhelika Shakurova <anzelikasht@gmail.com>
2020-10-30 23:14:15 +0300

    add swagger

--- src/main/java/com/anzhelika/spring_boot_application/configuration/BeanConfiguration.java
+++ src/main/java/com/anzhelika/spring_boot_application/configuration/BeanConfiguration.java
@@ -3,6 +3,10 @@ package com.anzhelika.spring_boot_application.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class BeanConfiguration {
@@ -10,5 +14,14 @@ public class BeanConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
